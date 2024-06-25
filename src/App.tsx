1d81467595cc28756bd3378c62b9982224640dbe
okusmierek <okusmierek@automotivemastermind.com>
2024-06-25 19:10:09 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div css={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
       <Typography size="3xl" weight="bold">
-        Hello, It's Admin Panel !!!!!
+        Hello, Its Admin Panel !!!!
       </Typography>
     </div>
   );
